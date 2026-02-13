aeeefbb877cfc5d6906f1bd408ccd5bbc5b98a5f
Ayman <aymanamsrour0@com>
2026-02-14 00:05:27 +0100

    Fix langchain.schema import to langchain_core.documents

--- llama.py
+++ llama.py
@@ -17,7 +17,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.schema import Document as LangchainDocument
+from langchain_core.documents import Document as LangchainDocument
 from sentence_transformers import CrossEncoder
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
